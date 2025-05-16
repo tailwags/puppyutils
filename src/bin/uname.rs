27e37fb832e659b322db37c8896606063a5e06b9
Matilde Morrone <contact@morrone.dev>
2025-05-16 14:08:10 +0100

    Add a basic uname implementation

--- src/bin/uname.rs
+++ src/bin/uname.rs
@@ -1,1 +1,28 @@
-fn main() {}
+use std::io::{Write, stdout};
+
+use coreutils::Result;
+use rustix::system::uname;
+
+fn main() -> Result {
+    let uname = uname();
+
+    let mut stdout = stdout();
+
+    stdout.write_all(uname.sysname().to_bytes())?;
+    stdout.write_all(b" ")?;
+
+    stdout.write_all(uname.nodename().to_bytes())?;
+    stdout.write_all(b" ")?;
+
+    stdout.write_all(uname.release().to_bytes())?;
+    stdout.write_all(b" ")?;
+
+    stdout.write_all(uname.version().to_bytes())?;
+    stdout.write_all(b" ")?;
+
+    stdout.write_all(uname.machine().to_bytes())?;
+
+    stdout.write_all(b"\n")?;
+
+    Ok(())
+}
