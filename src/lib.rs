f264302e8d3f3535c516d590c9d90f50af750b28
Matilde Morrone <contact@morrone.dev>
2025-05-16 14:02:10 +0100

    Implement a common exit code

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,35 @@
+use std::{fmt::Debug, io, process::ExitCode};
 
+pub type Result<T = (), E = Exit> = std::result::Result<T, E>;
+
+pub enum Exit {
+    IoError(io::Error),
+    Code(ExitCode),
+}
+
+impl Debug for Exit {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::IoError(err) => err.fmt(f),
+            Self::Code(code) => code.fmt(f),
+        }
+    }
+}
+
+impl From<io::Error> for Exit {
+    fn from(err: io::Error) -> Self {
+        Self::IoError(err)
+    }
+}
+
+impl From<ExitCode> for Exit {
+    fn from(code: ExitCode) -> Self {
+        Self::Code(code)
+    }
+}
+
+impl From<()> for Exit {
+    fn from(_: ()) -> Self {
+        Self::Code(ExitCode::SUCCESS)
+    }
+}
