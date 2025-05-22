6ef9766b02ce8426dfbb197f6e29de7efb95ef8d
esther-ff <creepermaster000@gmail.com>
2025-05-22 22:41:28 +0200

    more parsing, cleanups

--- src/bin/ls.rs
+++ src/bin/ls.rs
@@ -4,7 +4,7 @@
 use bitflags::{Flags, bitflags};
 use coreutils::Result;
 use rustix::{
-    fs::{Dir, Mode, OFlags, open},
+    fs::{Dir, Mode, OFlags, open, setxattr},
     termios::tcgetwinsize,
 };
 use sap::{
@@ -16,39 +16,252 @@ use std::io::{BufWriter, Write, stdout};
 const DEFAULT_BLOCK_SIZE: usize = 512;
 
 bitflags! {
+    #[rustfmt::skip]
     struct LsFlags: u32 {
-        const NOT_IGNORE_DOTS = 1 << 0; // -a --all
-        const IGNORE_DOTS_EXCEPT_DIRS = 1 << 1; // -A --almost-all
-        const PRINT_AUTHOR = 1 << 2; // --author
-        const LIST_DIRECTORIES = 1 << 3; // -d -- directory
-        const C_STYLE_ESCAPED = 1 << 4; // -b --escape
-        const IGNORE_TILDE_ENTRIES = 1 << 5; // -B --ignore-backups
-        const LIST_BY_COLUMNS = 1 << 6; // -C
-        const EMACS_DIRED_MODE = 1 << 7; // -D --dired
-        const SORT_ENTRIES = 1 << 8; // partially -f (can disable it) and -U
-        const NO_OWNER_LISTED = 1 << 9; // related to -g
-        const NO_GROUPS_LISTED = 1 << 10; // -G --no-group
-        const HUMAN_READABLE_SIZES = 1 << 11; // -h --human-readable
-        const SI_SIZES = 1 << 12; // --si
+        const NOT_IGNORE_DOTS =          1 << 0;  // -a --all
+        const IGNORE_DOTS_EXCEPT_DIRS =  1 << 1;  // -A --almost-all
+        const PRINT_AUTHOR =             1 << 2;  // --author
+        const LIST_DIRECTORIES =         1 << 3;  // -d -- directory
+        const C_STYLE_ESCAPED =          1 << 4;  // -b --escape
+        const IGNORE_TILDE_ENTRIES =     1 << 5;  // -B --ignore-backups
+        const LIST_BY_COLUMNS =          1 << 6;  // -C
+        const EMACS_DIRED_MODE =         1 << 7;  // -D --dired
+        const SORT_ENTRIES =             1 << 8;  // partially -f (can disable it) and -U
+        const NO_OWNER_LISTED =          1 << 9;  // related to -g
+        const NO_GROUPS_LISTED =         1 << 10; // -G --no-group
+        const HUMAN_READABLE_SIZES =     1 << 11; // -h --human-readable
+        const SI_SIZES =                 1 << 12; // --si
+        const PRINT_INODE_INDEXES =      1 << 13; // -i --inode
+        const KB_BLOCKS =                1 << 14; // -k --kibibytes
+        const GROUP_DIRS_FIRST =         1 << 15; // --group-directories-first
+        const DEREF_SYMLINKS =           1 << 16; // -L --dereference
+        const COMMA_SEP_LIST =           1 << 17; // -m
+        const NUMERIC_IDS =              1 << 18; // -n --numeric-uid-gid
+        const LITERAL_NAMES =            1 << 19; // -N --literal
+        const REVERSE_SORT =             1 << 20; // -r, --reverse
+        const RECURSIVE =                1 << 21; // -R --recursive
+        const PRINT_ALLOCATED_SIZE =     1 << 22; // -s --size
+        const SORT_BY_VERSION_NUMBER =   1 << 23; // -v
+        const LIST_BY_LINES =            1 << 24; // -x
+        const PRINT_SECURIT_CTXT =       1 << 25; // -Z --context
+        const END_WITH_NUL =             1 << 26; // --zero
+        const ONE_FILE_PER_LINE =        1 << 27; // -1
 
-        // maybe -H will be here.
-
-        const PRINT_INODE_INDEXES = 1 << 13; // -i --inode
-        const KB_BLOCKS = 1 << 14; // -k --kibibytes
-        const LONG_LISTING = 1 << 15; // -l
-        const DEREF_SYMLINKS = 1 << 16; // -L --dereference
-        const COMMA_SEP_LIST = 1 << 17; // -m
-        const NUMERIC_IDS = 1 << 18; // -n --numeric-uid-gid
-        const LITERAL_NAMES = 1 << 19; // -N --literal
-        const REVERSE_SORT = 1 << 20; // -r, --reverse
-        const RECURSIVE = 1 << 21; // -R --recursive
-        const PRINT_ALLOCATED_SIZE = 1 << 22; // -s --size
-        const SORT_BY_VERSION_NUMBER = 1 << 23; // -v
-        const LIST_BY_LINES = 1 << 24; // -x
-        const PRINT_SECURIT_CTXT = 1 << 25; // -Z --context
-        const END_WITH_NUL = 1 << 26; // --zero
-        const ONE_FILE_PER_LINE = 1 << 27; // -1
+        /// refers to the `-c` option
+        /// as it needs to be evaluated last.
+        const LOWERCASE_C =              1 << 28;
     }
+}
+
+fn parse_arguments() -> Result<(LsConfig, bool)> {
+    let mut any_args = false;
+    let mut args = Parser::from_env()?;
+    let mut settings = LsConfig {
+        flags: LsFlags::empty(),
+        order: SortOrder::Name,
+        time_ty: TimeStampType::Locale,
+        quoting: QuotingStyle::Literal,
+        indicator: IndicatorStyle::None,
+        deref: Dereference::None,
+        color: When::Always,
+        hyperlink_file_names: When::Always,
+        classify_files: When::Always,
+        dir: None,
+        blk_size: DEFAULT_BLOCK_SIZE,
+        format: Formatting::Horizontal, // default seems to be horizontal
+        width: get_win_size().ws_col,
+    };
+
+    while let Some(arg) = args.forward()? {
+        if !any_args {
+            any_args = true;
+        }
+
+        match arg {
+            Short('a') | Long("all") => {
+                settings.flags |= LsFlags::NOT_IGNORE_DOTS;
+            }
+
+            Short('A') | Long("almost-all") => {
+                settings.flags |= LsFlags::IGNORE_DOTS_EXCEPT_DIRS;
+            }
+
+            Long("author") => {
+                settings.flags |= LsFlags::PRINT_AUTHOR;
+            }
+
+            Short('b') | Long("escape") => {
+                settings.flags |= LsFlags::C_STYLE_ESCAPED;
+            }
+
+            Long("block-size") => {
+                if let Some(arg) = args.value() {
+                    // do the block-size
+                } else {
+                    needs_an_argument();
+                }
+            }
+
+            Short('B') | Long("ignore-backups") => {
+                settings.flags |= LsFlags::IGNORE_TILDE_ENTRIES;
+            }
+
+            Short('c') => {
+                settings.flags |= LsFlags::LOWERCASE_C;
+            }
+
+            Short('C') => {
+                settings.flags |= LsFlags::LIST_BY_COLUMNS;
+            }
+
+            Long("color") => {
+                if let Some(value) = args.value() {
+                    match When::from_bytes(&value) {
+                        None => invalid_argument(),
+                        Some(color) => settings.color = color,
+                    }
+                } else {
+                    needs_an_argument()
+                }
+            }
+
+            Short('d') | Long("directory") => settings.flags |= LsFlags::LIST_DIRECTORIES,
+
+            Short('D') | Long("dired") => settings.flags |= LsFlags::EMACS_DIRED_MODE,
+
+            Short('f') => {
+                settings.flags &= !LsFlags::SORT_ENTRIES;
+                settings.flags |= LsFlags::NOT_IGNORE_DOTS;
+
+                settings.color = When::Never;
+                settings.order = SortOrder::Directory;
+            }
+
+            Short('F') | Long("classify") => {
+                settings.classify_files = match args.value() {
+                    None => needs_an_argument(),
+                    Some(val) => match When::from_bytes(&val) {
+                        None => invalid_argument(),
+                        Some(str) => str,
+                    },
+                };
+            }
+
+            Long("file-type") => {
+                settings.classify_files = match args.value() {
+                    None => needs_an_argument(),
+                    Some(val) => match When::from_bytes(&val) {
+                        None => invalid_argument(),
+                        Some(str) => str,
+                    },
+                };
+
+                // this should make so '*' doesn't appear
+            }
+
+            Long("format") => {
+                settings.format = match args.value() {
+                    None => needs_an_argument(),
+                    Some(val) => match Formatting::from_bytes(&val) {
+                        None => invalid_argument(),
+
+                        Some(x) => x,
+                    },
+                };
+            }
+
+            Long("full-time") => {
+                settings.format = Formatting::Long;
+                settings.time_ty = TimeStampType::FullIso;
+            }
+
+            Short('g') => {
+                settings.flags |= LsFlags::NO_OWNER_LISTED;
+                settings.format = Formatting::Long;
+            }
+
+            Long("group-directories-first") => {
+                settings.flags |= LsFlags::GROUP_DIRS_FIRST;
+            }
+
+            Short('G') | Long("no-group") => {
+                settings.flags |= LsFlags::NO_GROUPS_LISTED;
+            }
+
+            Short('h') | Long("human-readable") => {
+                settings.flags |= LsFlags::HUMAN_READABLE_SIZES;
+            }
+
+            Long("si") => {
+                settings.flags |= LsFlags::SI_SIZES;
+            }
+
+            Short('H') | Long("dereference-command-line") => {
+                // not sure how to handle this
+                // requires some testing.
+            }
+
+            Long("derefence-command-line-symlinks") => {
+                // above
+            }
+
+            Long("hide") => {
+                todo!("--hide")
+            }
+
+            Long("hyperlink") => {
+                settings.hyperlink_file_names = match args.value() {
+                    None => needs_an_argument(),
+                    Some(val) => match When::from_bytes(&val) {
+                        None => invalid_argument(),
+
+                        Some(x) => x,
+                    },
+                };
+            }
+
+            Long("indicator-style") => {
+                settings.indicator = match args.value() {
+                    None => needs_an_argument(),
+                    Some(val) => match IndicatorStyle::from_bytes(&val) {
+                        None => invalid_argument(),
+
+                        Some(x) => x,
+                    },
+                };
+            }
+
+            Short('i') | Long("inode") => {
+                settings.flags |= LsFlags::PRINT_INODE_INDEXES;
+            }
+
+            Short('I') | Long("ignore") => {
+                // todo
+                todo!()
+            }
+
+            Short('l') => {
+                settings.format = Formatting::Long;
+            }
+
+            Short('L') | Long("dereference") => {
+                settings.flags |= LsFlags::DEREF_SYMLINKS;
+            }
+
+            Short('m') => {
+                settings.flags |= LsFlags::COMMA_SEP_LIST;
+            }
+
+            Short('n') | Long("numeric-uid-gid") => {
+                settings.flags |= LsFlags::NUMERIC_IDS;
+            }
+            _ => {
+                // todo!
+            }
+        }
+    }
+
+    Ok((settings, any_args))
 }
 
 enum SortOrder {
@@ -57,6 +270,7 @@ enum SortOrder {
     Size,
     Version,
     Extension,
+    Directory,
     Width,
     AccessTime,
     Time(TimeStampType),
@@ -89,12 +303,71 @@ enum IndicatorStyle {
     FileType,
     Classify,
 }
+impl IndicatorStyle {
+    #[inline]
+    fn from_bytes<A>(val: A) -> Option<Self>
+    where
+        A: AsRef<[u8]>,
+    {
+        match val.as_ref() {
+            b"none" => Some(Self::None),
+            b"slash" => Some(Self::Slash),
+            b"file-type" => Some(Self::FileType),
+            b"classify" => Some(Self::Classify),
+
+            _ => None,
+        }
+    }
+}
+
+#[repr(u8)]
+enum Formatting {
+    Long,
+    Horizontal,
+    Across,
+    Commas,
+    SingleCol,
+}
+
+impl Formatting {
+    #[inline]
+    fn from_bytes<A>(val: A) -> Option<Self>
+    where
+        A: AsRef<[u8]>,
+    {
+        match val.as_ref() {
+            b"verbose" | b"long" => Some(Self::Long),
+            b"horizontal" => Some(Self::Horizontal),
+            b"across" => Some(Self::Across),
+            b"commas" => Some(Self::Commas),
+            b"single-column" => Some(Self::Commas),
+
+            _ => None,
+        }
+    }
+}
 
 #[repr(u8)]
 enum When {
     Never,
     Auto,
     Always,
+}
+
+impl When {
+    #[inline]
+    fn from_bytes<A>(val: A) -> Option<Self>
+    where
+        A: AsRef<[u8]>,
+    {
+        match val.as_ref() {
+            b"always" | b"yes" | b"force" => Some(Self::Always),
+            b"never" | b"no" | b"none" => Some(Self::Never),
+            b"auto" | b"tty" | b"if-tty" => Some(Self::Auto),
+
+            _ => None,
+        }
+    }
 }
 
 #[repr(u8)]
@@ -145,102 +418,19 @@ struct LsConfig {
     // block size
     blk_size: usize,
 
+    // formatting used
+    format: Formatting,
+
     // line width.
     width: u16,
 }
 
 const CURRENT_DIR_PATH: &str = ".";
 
-#[repr(u8)]
-enum FileType {
-    Regular = 0,
-    Directory = 1,
-    BlockDevice = 2,
-    SymbolicLink = 3,
-    Socket = 4,
-    CharacterSpecial = 5,
-    Fifo = 6,
-}
-
-impl FileType {
-    fn get_letter(&self) -> &'static str {
-        use FileType::*;
-
-        match self {
-            Regular => "-",
-            Directory => "d",
-            BlockDevice => "b",
-            SymbolicLink => "l",
-            Socket => "s",
-            CharacterSpecial => "c",
-            Fifo => "p",
-        }
-    }
-}
-
-struct Permissions(u16);
-
-impl Permissions {
-    fn new(perms: u16) -> Option<Self> {
-        if perms > 511 { None } else { Some(Self(perms)) }
-    }
-}
-
 fn main() -> Result {
     println!("size_of::<LsConfig>() => {}", size_of::<LsConfig>());
-    let mut any_args = false;
-    let mut args = Parser::from_env()?;
 
-    let mut settings = LsConfig {
-        flags: LsFlags::empty(),
-        order: SortOrder::Name,
-        time_ty: TimeStampType::Locale,
-        quoting: QuotingStyle::Literal,
-        indicator: IndicatorStyle::None,
-        deref: Dereference::None,
-        color: When::Always,
-        hyperlink_file_names: When::Always,
-        classify_files: When::Always,
-        dir: None,
-        blk_size: DEFAULT_BLOCK_SIZE,
-        width: get_win_size().ws_col,
-    };
-
-    while let Some(arg) = args.forward()? {
-        if !any_args {
-            any_args = true;
-        }
-
-        match arg {
-            Short('a') | Long("all") => {
-                settings.flags |= LsFlags::NOT_IGNORE_DOTS;
-            }
-
-            Short('A') | Long("almost-all") => {
-                settings.flags |= LsFlags::IGNORE_DOTS_EXCEPT_DIRS;
-            }
-
-            Long("author") => {
-                settings.flags |= LsFlags::PRINT_AUTHOR;
-            }
-
-            Short('b') | Long("escape") => {
-                settings.flags |= LsFlags::C_STYLE_ESCAPED;
-            }
-
-            Long("block-size") => {
-                if let Some(arg) = args.value() {
-                    // do the block-size
-                } else {
-                    // error out
-                }
-            }
-
-            _ => {
-                // todo!
-            }
-        }
-    }
+    let (settings, any_args) = parse_arguments()?;
 
     if any_args {
         todo!()
@@ -248,7 +438,6 @@ fn main() -> Result {
         // We received no arguments
         // therefore we can do the most "basic" action
         // just listing contents of the current directory.
-
         let fd = open(
             CURRENT_DIR_PATH,
             OFlags::DIRECTORY | OFlags::RDONLY,
@@ -269,6 +458,14 @@ fn main() -> Result {
     }
 
     Ok(())
+}
+
+fn needs_an_argument() -> ! {
+    todo!()
+}
+
+fn invalid_argument() -> ! {
+    todo!()
 }
 
 fn get_win_size() -> rustix::termios::Winsize {
