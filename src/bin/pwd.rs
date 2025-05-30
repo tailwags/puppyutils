bb50eaf9b9d48cb28cbf3d0130b850166f076bc2
Matilde Morrone <contact@morrone.dev>
2025-05-30 16:36:48 +0200

    Add a naive pwd implementation

--- src/bin/pwd.rs
+++ src/bin/pwd.rs
@@ -1,9 +1,15 @@
-use std::io::stdout;
+use std::io::{Write, stdout};
 
 use puppyutils::{Result, cli};
+use rustix::process::getcwd;
 fn main() -> Result {
     let mut stdout = stdout();
     cli!("pwd", stdout, #error);
+
+    let p = getcwd(Vec::new())?;
+
+    stdout.write_all(p.as_bytes())?;
+    stdout.write_all(b"\n")?;
 
     Ok(())
 }
