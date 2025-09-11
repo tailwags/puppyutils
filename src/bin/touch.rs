fb9e747cfdb891eb352c83f2ee2c450d2072dc17
Matilde Morrone <contact@morrone.dev>
2025-09-11 23:36:37 +0200

    Add support for m and a flags for touch command

--- src/bin/touch.rs
+++ src/bin/touch.rs
@@ -2,7 +2,8 @@ use std::{ffi::CStr, io::stdout};
 
 use puppyutils::{Result, cli};
 use xenia::{
-    AtFlags, ClockId, Errno, Mode, OFlags, Timestamps, clock_gettime, open, stdio::cwd, utimensat,
+    AtFlags, ClockId, Errno, Mode, OFlags, Timespec, Timestamps, clock_gettime, open, stdio::cwd,
+    utimensat,
 };
 
 pub fn main() -> Result {
@@ -10,20 +11,29 @@ pub fn main() -> Result {
     let mut files = Vec::new();
 
     let mut no_create = false;
+    let mut access_time = true;
+    let mut modification_time = true;
 
     cli! {
         "touch", stdout, #error
         Short('c') | Long("no-create") => no_create = true
+        Short('a') => modification_time = false
+        Short('m')=> access_time = false
         Value(value) => {
             files.push(value.into_owned());
         }
     };
 
-    let now = clock_gettime(ClockId::Realtime)?;
+    let now = if files.len() == 1 {
+        Timespec::NOW
+    } else {
+        clock_gettime(ClockId::Realtime)?
+    };
 
+    #[rustfmt::skip]
     let timestamps = Timestamps {
-        last_access: now,
-        last_modification: now,
+        last_access: if access_time { now } else { Timespec::OMIT },
+        last_modification: if modification_time { now } else { Timespec::OMIT },
     };
 
     for file in files {
